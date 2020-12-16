b89fa239fc2734b87ca4775d975127621366c057
MartiLlorach <73992582+MartiLlorach@users.noreply.github.com>
2020-12-16 21:54:22 +0100

    Add files via upload

--- JUEGO+INFORMES.py
+++ JUEGO+INFORMES.py
@@ -18,9 +18,11 @@ db = pymysql.connect(conexion, usuario, password, BBDD)
 query1='WITH MyRowSet AS (select idparticipante,carta_inicial,count(carta_inicial) as "usos",ROW_NUMBER() OVER (PARTITION BY idparticipante) AS "primera_carta" from turnos group by idparticipante,carta_inicial) SELECT * FROM MyRowSet WHERE Primera_carta = 1;'
 query2='select nombre,max(apuesta),idpartida from (select case when username is not null then usuario.username else descripcion end as nombre,max(turnos.apuesta) as apuesta,partida.idpartida as idpartida from jugador left join bot on bot.idbot=jugador.idbot left join usuario on usuario.idusuario=jugador.idusuario inner join participante on jugador.idjugador=participante.id_jugador inner join turnos on participante.id_participante=turnos.idparticipante inner join partida on turnos.idpartida=partida.idpartida where turnos.apuesta is not null group by partida.idpartida,username) tabla where (apuesta,idpartida) in (select max(turnos.apuesta),partida.idpartida as apuesta from jugador left join bot on bot.idbot=jugador.idbot left join usuario on usuario.idusuario=jugador.idusuario inner join participante on jugador.idjugador=participante.id_jugador inner join turnos on participante.id_participante=turnos.idparticipante inner join partida on turnos.idpartida=partida.idpartida group by partida.idpartida order by max(turnos.apuesta) desc) group by idpartida;'
 query3='select nombre,min(apuesta),idpartida from (select case when username is not null then usuario.username else descripcion end as nombre,min(turnos.apuesta) as apuesta,partida.idpartida as idpartida from jugador left join bot on bot.idbot=jugador.idbot left join usuario on usuario.idusuario=jugador.idusuario inner join participante on jugador.idjugador=participante.id_jugador inner join turnos on participante.id_participante=turnos.idparticipante inner join partida on turnos.idpartida=partida.idpartida where turnos.apuesta is not null group by partida.idpartida,username) tabla where (apuesta,idpartida) in (select min(turnos.apuesta),partida.idpartida as apuesta from jugador left join bot on bot.idbot=jugador.idbot left join usuario on usuario.idusuario=jugador.idusuario inner join participante on jugador.idjugador=participante.id_jugador inner join turnos on participante.id_participante=turnos.idparticipante inner join partida on turnos.idpartida=partida.idpartida group by partida.idpartida order by min(turnos.apuesta) desc) group by idpartida;'
+query5='select distinct bot.descripcion,partida.ganador_partida, truncate(((2/sum(partida.idpartida))*100),2) as porcentaje from partida inner join participante on partida.ganador_partida=participante.id_participante inner join jugador on participante.id_jugador=jugador.idjugador inner join bot on jugador.idbot=bot.idbot where bot.idbot is not null;'
 query11='select avg(t.apuesta) as "Media", p.idpartida from turnos t inner join partida p on p.idpartida = t.idpartida group by idpartida;'
+query12='select distinct u.*,apuesta , max(t.numero_turno) as "turno_max", pd.idpartida from usuario u inner join jugador j on u.idusuario = j.idusuario inner join participante p on j.idjugador = p.id_jugador inner join turnos t on p.id_participante = t.idparticipante inner join partida pd on pd.idpartida = t.idpartida group by idpartida,idparticipante -- having numero_turno in  (select max(numero_turno) from turnos group by idpartida) order by t.idpartida asc,idparticipante asc, numero_turno asc'
 query13='select count(carta_inicial) AS numero_de_cartas, sum(valor) AS valor_total, idpartida AS partida from turnos, cartas where carta_inicial=idcartas group by idpartida;'
-querys=[query1,query2,query3,query11,query13]
+querys=[query1,query2,query3,query5,query11,query12,query13]
 
 def query_to_xml(query_sql):
     outfile.write('<query>\n')
@@ -503,8 +505,7 @@ while True:
         pausa()
         jugador = {}
         for i in jugadores:
-            jugador[i] = ['1ra_carta', 'estado mano actual', 'estado partida', 'prioridad', 'valor cartas', 0,
-                          init_points, 'contador mano', ]
+            jugador[i] = ['1ra_carta', 'estado mano actual', 'estado partida', 'prioridad', 'valor cartas', 0,init_points, 'contador mano', ]
         contmano = 0
         while contmano < max_r and partida:
             maxima_puntuacion = 0
@@ -530,7 +531,11 @@ while True:
         outfile.close()
 
         while True:
-            n = input_int('Que informe quieres ver?\n1- \n2- \n3- \n4- \n5- \n')
+            while True:
+                n = input_int('Que informe quieres ver?\n1- Mostrar la Carta inicial más repetida por cada jugador\n2- Jugador que realiza la apuesta más alta por partida\n3- Jugador que realiza apuesta más baja por partida\n4- Porcentaje de partidas ganadas Bots en general. \n5- Calcular la apuesta media por partida\n6- Mostrar los datos de los usuarios que no son bot, así como cual ha sido su última apuesta en cada partida que ha jugado\n7- Calcular el valor total de las cartas y el numero total de cartas que se han dado inicialmente en las manos en la partida\n')
+                lista_q=[1,2,3,4,5,6,7]
+                if n in lista_q:
+                    break
             cursor = db.cursor()
             cursor.execute(querys[n - 1])
             rows = cursor.fetchall()
